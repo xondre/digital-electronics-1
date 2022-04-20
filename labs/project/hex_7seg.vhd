12ac2771a0743fa96a73e83a9ef8efe769e8ba7c
xondre <99501336+xondre@users.noreply.github.com>
2022-04-20 15:57:02 +0100

    Add files via upload

---
+++ labs/project/hex_7seg.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/20/2022 01:50:15 PM
+-- Design Name: 
+-- Module Name: hex_7seg - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity hex_7seg is
+--  Port ( );
+end hex_7seg;
+
+architecture Behavioral of hex_7seg is
+
+begin
+
+
+end Behavioral;
